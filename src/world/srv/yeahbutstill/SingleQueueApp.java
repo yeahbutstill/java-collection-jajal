34ce2c3116cd24394c5d313f4a530362f5761df5
yeahbutstill <mayadani28@gmail.com>
2021-04-23 04:39:12 +0700

    sample abstract collection single queue

---
+++ src/world/srv/yeahbutstill/SingleQueueApp.java
@@ -0,0 +1,15 @@
+package world.srv.yeahbutstill;
+
+import world.srv.yeahbutstill.collection.SingleQueue;
+
+import java.util.Queue;
+
+public class SingleQueueApp {
+
+    public static void main(String[] args) {
+
+        Queue<String> queue = new SingleQueue<>();
+
+    }
+
+}
---
+++ src/world/srv/yeahbutstill/collection/SingleQueue.java
@@ -0,0 +1,2 @@
+package world.srv.yeahbutstill.collection;public class SingleQueue {
+}
